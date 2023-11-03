1cb0748cdc94a9186fa61ce0e1458ff4df433fe8
KimLien <6251071052@st.utc2.edu.vn>
2023-11-03 18:25:24 +0700

    RS UP

--- WebQuanLyThuVien/Areas/Admin/Controllers/PhieuMuonController.cs
+++ WebQuanLyThuVien/Areas/Admin/Controllers/PhieuMuonController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebQuanLyThuVien.Areas.Admin.Data;
+using WebQuanLyThuVien.Areas.Admin.Services;
+using WebQuanLyThuVien.Interfaces.Services;
+using WebQuanLyThuVien.Models;
+using WebQuanLyThuVien.Services;
 
 namespace WebQuanLyThuVien.Areas.Admin.Controllers
 {
     public class PhieuMuonController : Controller
     {
+        SachService _sachService = new SachService();
+        DocGiaService _docGiaService = new DocGiaService();
         // GET: Admin/PhieuMuon
         public ActionResult Index()
         {
-            return View();
+            if (Session["user"] == null)
+                return RedirectToAction("Login", "Account");
+            else
+            {
+                var sach = _sachService.GetAll();
+                var docGia = _docGiaService.GetAllDocGia_TheDocGia();
+
+                ViewData["Sach"] = sach;
+                ViewData["DocGia"] = docGia;
+
+                return View();
+            }
         }
     }
 }
